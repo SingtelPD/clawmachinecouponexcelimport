3c1c28f18a3be2ca29a053575491c266deed0c46
SingtelPD <71814369+SingtelPD@users.noreply.github.com>
2022-10-14 15:24:20 +0800

    Update coupon_code_excel_import_streamlit_cloud.py

--- coupon_code_excel_import_streamlit_cloud.py
+++ coupon_code_excel_import_streamlit_cloud.py
@@ -13,7 +13,7 @@ options = Options()
 options.headless = True
 
 st.title("5G Claw Machine CMS - input coupon code from excel file")
-st.write("Please use the form below to enter the codes into the CMS system. Note: the form below accepts coupon codes that are whole numbers only.")
+st.write("Please use the form below to enter the codes into the CMS system.")
 st.write("If at any point you encounter an error message, this could be due to the following: 1) username and/or password is incorrrect; or 2) coupon code already exists in the CMS system.")
 
 uploaded_file = st.file_uploader("Upload file", type=['xlsx'])
