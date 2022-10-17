2cea87bce3007169209768351377d8cffdbf8334
SingtelPD <71814369+SingtelPD@users.noreply.github.com>
2022-10-17 12:16:43 +0800

    Update coupon_code_excel_import_streamlit_cloud.py

--- coupon_code_excel_import_streamlit_cloud.py
+++ coupon_code_excel_import_streamlit_cloud.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.headless = True
 
-st.title("🕹️ 5G Claw Machine CMS - input coupon code from excel file")
+st.title("🕹️ 5G Claw Machine CMS - input game codes from excel file")
 st.write("Please use the form below to enter the codes into the CMS system.")
 st.write("If at any point you encounter an error message, this could be due to the following: 1) username and/or password is incorrrect; or 2) coupon code already exists in the CMS system.")
 
